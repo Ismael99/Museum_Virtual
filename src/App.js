ebcad2aef039f899ace70859274e71af77ea0765
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-11-20 22:21:12 -0600

    home

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import React from 'react'
-import View from './components/View'
-import img_prueba from './images/museum.jpg'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NavbarContainer from './pages/NavbarContainer'
 
-const App = () => {
-    return(
 
-        <div className='container mt-4'>
-            <h1>Museo Virtual</h1>
-
-            <View
-                image={img_prueba}
-                category="Autor Name"
-                description= "Sit velit odio ratione accusantium necessitatibus. Dolorum corrupti commodi architecto eos esse unde. Dolorum et non voluptate animi deleniti possimus quam necessitatibus. Placeat consectetur sunt sequi vitae reprehenderit Libero odio nemo. Elit esse dolor culpa sunt deleniti nulla, saepe odit saepe vero repudiandae. Exercitationem a ex iste ullam quisquam Beatae deleniti doloribus iusto provident id. Perspiciatis velit laudantium incidunt temporibus voluptate. Voluptatibus non "
-            />
-        </div>
-    )
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={NavbarContainer} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/pages/NavbarContainer.js
@@ -0,0 +1,47 @@
+import React, {Fragment} from 'react'
+import { Switch, Route, Link, useRouteMatch } from "react-router-dom";
+import Home from '../components/Home'
+
+import {Navbar, NavDropdown, Nav} from 'react-bootstrap'
+
+const NavbarContainer = () => {
+      // RouteMatch
+        const match = useRouteMatch();
+
+    return (
+            <Fragment>
+
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                  <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                      <Nav.Link href="#features">Features</Nav.Link>
+                      <Nav.Link href="#pricing">Pricing</Nav.Link>
+                      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                      </NavDropdown>
+                    </Nav>
+                    <Nav>
+                      <Nav.Link href="#deets">More deets</Nav.Link>
+                      <Nav.Link eventKey={2} href="#memes">
+                        Dank memes
+                      </Nav.Link>
+                    </Nav>
+                  </Navbar.Collapse>
+              </Navbar>
+                <Switch>
+                  {/*<!-- Es necesario que la ruta padre contenga un exact path --> */}
+                  <Route exact path={match.path}>
+                    <Home />
+                  </Route>
+                </Switch>
+            </Fragment>
+    )
+}
+
+export default NavbarContainer
