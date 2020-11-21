3847cfe7ab09e2d41c54f16c88b2f09fd8982ad5
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-11-20 19:23:13 -0600

    vercel_test5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, {Fragment} from 'react'
 
 const App = () => {
-    <Fragment>
-      <h1>Museo Virtual</h1>  
-    </Fragment>
+    return(
+
+        <Fragment>
+          <h1>Museo Virtual</h1>  
+        </Fragment>
+    )
 }
 
 export default App;
