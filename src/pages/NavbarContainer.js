b3e47cf63a8873bf5b8f6e6c1f0a1d9c4f3d5e2b
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-11-20 22:23:16 -0600

    home

--- src/pages/NavbarContainer.js
+++ src/pages/NavbarContainer.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react'
-import { Switch, Route, Link, useRouteMatch } from "react-router-dom";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
 import Home from '../components/Home'
 
 import {Navbar, NavDropdown, Nav} from 'react-bootstrap'
