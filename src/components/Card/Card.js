ed2729e41375dd98f8a5d4b64cbfdf7871c58621
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-11-21 17:39:11 -0600

    plantilla

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -12,13 +12,13 @@ const Cards= (props) =>{
                 height: '30rem',
             }}>
                 <Card.Img variant="top" src={image} />
-            <Card.Body>
-            <Card.Title>{autor}</Card.Title>
-            <Card.Text>
-                {description}
-            </Card.Text>
-            <Link to={url} variant="primary">Obras</Link>
-          </Card.Body>
+                <Card.Body>
+                <Card.Title className='Card-title' >{autor}</Card.Title>
+                <Card.Text className='Card-description'>
+                    {description}
+                </Card.Text>
+                <Link className='Card-link' to={url} variant="primary">Obras</Link>
+            </Card.Body>
         </Card>
     )
 }
